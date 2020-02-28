d5c277bf5fa11a0ed8f60c36877fe6c3b11b909c
Justin Sandahl <sandahl.j.d@gmail.com>
2020-02-28 14:30:13 +0000

    cleaned up code, no structural changes just format and spacing

--- myscripts.js
+++ myscripts.js
@@ -1,33 +1,33 @@
-const newForm = document.getElementById("myForm");       
+const newForm = document.getElementById("myForm");
 
-const submitClicked = document.getElementById("btnSubmit");       
-const resetClicked = document.getElementById("btnReset"); 
+const submitClicked = document.getElementById("btnSubmit");
+const resetClicked = document.getElementById("btnReset");
 
 submitClicked.addEventListener("click", NewSubmission);
 resetClicked.addEventListener("click", ConfirmReset);
 
-function NewSubmission(){
-    const newName = document.getElementById("name").value;
-    const newEmail = document.getElementById("mail").value;
-    const newMessage = document.getElementById("msg").value;
-    
-    if (newName == "" || newEmail == "" || newMessage == ""){
-        alert("Please complete all fields");
-        return false;
-    } else {
-        alert(newName + ", " + newEmail + ", " + newMessage);
-        newForm.submit();
+function NewSubmission() {
+  const newName = document.getElementById("name").value;
+  const newEmail = document.getElementById("mail").value;
+  const newMessage = document.getElementById("msg").value;
 
-        /*window.open('','formConfirmation', 'width=400, height=400, resizable,scrollbars');
+  if (newName == "" || newEmail == "" || newMessage == "") {
+    alert("Please complete all fields");
+    return false;
+  } else {
+    alert(newName + ", " + newEmail + ", " + newMessage);
+    newForm.submit();
+
+    /*window.open('','formConfirmation', 'width=400, height=400, resizable,scrollbars');
         form.target = 'formConfirmation';*/
-    }
+  }
 }
 
-function ConfirmReset(){
-    if (confirm("Do you want to start over?")){
-        newForm.reset();
-        alert('Form reset');
-    }else {
-        return false;
-    }
+function ConfirmReset() {
+  if (confirm("Do you want to start over?")) {
+    newForm.reset();
+    alert("Form reset");
+  } else {
+    return false;
+  }
 }
